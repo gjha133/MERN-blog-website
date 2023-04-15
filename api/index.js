c67d806fec6a0bf44af6ce50353dbb086c56d194
Gautam Jha <gjha133@gmail.com>
2023-04-15 13:01:26 +0530

    Refactor index.js

--- api/index.js
+++ api/index.js
@@ -35,35 +35,49 @@ const salt = bcrypt.genSaltSync(10)
 const secret = process.env.SECRET
 
 app.post('/register', async (req, res) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
+
   try {
+    const hashedPassword = await bcrypt.hash(password, salt);
     const userDoc = await User.create({
       username,
-      password: bcrypt.hashSync(password, salt),
-    })
-    res.json(userDoc)
-  } catch (e) {
-    res.status(400).json(e)
+      password: hashedPassword,
+    });
+
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
   }
-})
+});
+
 
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body
-  const userDoc = await User.findOne({ username })
-  const passOk = bcrypt.compareSync(password, userDoc.password)
-  if (passOk) {
-    // logged in
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err
-      res.cookie('token', token).json({
-        id: userDoc._id,
-        username,
-      })
-    })
-  } else {
-    res.status(400).json('Wrong Credentials')
+  const { username, password } = req.body;
+
+  try {
+    const userDoc = await User.findOne({ username });
+
+    if (!userDoc) {
+      return res.status(400).json('User not found');
+    }
+
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (!passOk) {
+      return res.status(400).json('Wrong password');
+    }
+
+    const token = jwt.sign({ username, id: userDoc._id }, secret);
+    res.cookie('token', token).json({
+      id: userDoc._id,
+      username,
+    });
+
+  } catch (err) {
+    res.status(500).json('Server error');
   }
-})
+});
+
 
 app.get('/profile', (req, res) => {
   const { token } = req.cookies
@@ -76,62 +90,74 @@ app.get('/profile', (req, res) => {
 })
 
 app.post('/logout', (req, res) => {
-  res.cookie('token', '').json('ok')
-})
+  res.clearCookie('token').json('ok');
+});
 
 app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
-  const { originalname, path } = req.file
-  const parts = originalname.split('.')
-  const ext = parts[parts.length - 1]
-  const newPath = path + '.' + ext
-  fs.renameSync(path, newPath)
+  try {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
 
-  const { token } = req.cookies
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err
-    const { title, summary, content } = req.body
+    const { token } = req.cookies;
+    const info = await jwt.verify(token, secret);
+
+    const { title, summary, content } = req.body;
     const postDoc = await Post.create({
       title,
       summary,
       content,
       cover: newPath,
       author: info.id
-    })
-    res.json(postDoc)
-  })
+    });
 
-})
+    res.json(postDoc);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Something went wrong');
+  }
+});
+
 
 app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-  let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    newPath = path + '.' + ext;
-    fs.renameSync(path, newPath);
-  }
+  try {
+    let newPath = null;
+    if (req.file) {
+      const { originalname, path } = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path + '.' + ext;
+      fs.renameSync(path, newPath);
+    }
 
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
+    const { token } = req.cookies;
+    const info = jwt.verify(token, secret);
+
     const { id, title, summary, content } = req.body;
     const postDoc = await Post.findById(id);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+
     if (!isAuthor) {
-      return res.status(400).json('you are not the author');
+      return res.status(400).json('You are not the author');
     }
-    await postDoc.update({
+
+    const updatedPost = {
       title,
       summary,
       content,
       cover: newPath ? newPath : postDoc.cover,
-    });
+    };
 
-    res.json(postDoc);
-  });
-
+    const updatedPostDoc = await Post.findByIdAndUpdate(id, updatedPost, { new: true });
+    res.json(updatedPostDoc);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json('Internal server error');
+  }
 });
+
 
 
 app.get('/post', async (req, res) => {
@@ -143,10 +169,18 @@ app.get('/post', async (req, res) => {
 })
 
 app.get('/post/:id', async (req, res) => {
-  const { id } = req.params
-  const postDoc = await Post.findById(id).populate('author', ['username'])
-  res.json(postDoc)
+  try {
+    const { id } = req.params
+    const postDoc = await Post.findById(id).populate('author', ['username'])
+    if (!postDoc) {
+      return res.status(404).json({ error: 'Post not found' })
+    }
+    res.json(postDoc)
+  } catch (err) {
+    res.status(400).json({ error: 'Invalid ID' })
+  }
 })
+
 
 
 const PORT = process.env.PORT
